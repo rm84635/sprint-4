1d6eaaf9f0f9030679b88c0223d9f566b793eb65
RM84635 <rm84635@fiap.com.br>
2022-10-17 10:04:36 -0300

    Sobe a primeira versão da interface

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sprint 4
+
+### Pré-requisitos
+
+1. Ter o projeto da sprint 3 localmente (https://github.com/rm84635/sprint3)
+2. Instalar o NodeJS versão 16.18.0.
+3. Instalar as dependências desse projeto (execute `npm install`).
+
+### Como executar o projeto
+
+1. Execute o projeto da sprint 3 na porta 3000 em localhost.
+2. Execute `npm run start` para iniciar esse projeto.
+3. Acesse http://localhost:1337.
---
+++ index.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const express = require('express');
+const Mustache = require('mustache');
+const axios = require('axios');
+
+const app = express();
+const port = 1337;
+
+const baseURL = 'http://localhost:3000';
+
+function getEmails() {
+  return axios.get(`${baseURL}/email`);
+}
+
+function getProdutos() {
+  return axios.get(`${baseURL}/produto`);
+}
+
+function renderTemplate(name, data) {
+  return Mustache.render(
+    fs.readFileSync(`./src/templates/${name}.mustache`).toString(),
+    data
+  );
+}
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (_, res) => {
+  Promise.all([getEmails(), getProdutos()])
+    .then(function(results) {
+      const emails = results[0].data;
+      const produtos = results[1].data;
+
+      res.send(renderTemplate('index', {
+        emails: emails,
+        produtos: produtos
+      }));
+    });
+});
+
+app.post('/produto', (req, res) => {
+  axios.post(`${baseURL}/produto`, req.data);
+  res.redirect('/');
+});
+
+app.listen(port, () => {
+  console.log(`Executando na porta ${port}`);
+});
